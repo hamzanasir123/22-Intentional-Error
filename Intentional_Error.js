5641c4ebdff02b8875e5fafd7587cce7c4f99b40
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:34:48 +0500

    first commit

---
+++ Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+//If you haven’t received an array index error in one of your programs yet, try to make one happen.
+//Creating array.
+let newArray = ['Baber Azam', 'Virat Kholi', 'M.Rizwan'];
+//Accessing array by index.
+console.log(newArray[6]);
+// Make sure you correct the error before closing the program.
+//Correct The array.
+console.log(newArray);
